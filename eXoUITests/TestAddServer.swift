c3bbed284dc20ae29c76d948b698dafebb1a250a
Khemais <kmenzli@exoplatform.com>
2017-02-20 18:36:27 +0100

    MOB-2054: Upgrade the Swift iOS code to be able to release the project (Activate Add/edit Server config)

--- eXoUITests/TestAddServer.swift
+++ eXoUITests/TestAddServer.swift
@@ -9,35 +9,72 @@
 import XCTest
 
 class TestAddServer: eXoUIBaseTestCase {
-  
     
-    func gotoAddServerScreen () {
+    func testAddServer()  {
+        
         let app = XCUIApplication()
-        XCTAssertEqual(app.buttons.count, 4)
-        app.buttons["button.new.server"].tap()
-        //XCTAssertEqual(app.textViews.count,1)
-        //XCTAssertEqual(app.tables.count,1)
-    }
-   /**
-    func testAddServer() {
-        self.gotoAddServerScreen()
-        let app = XCUIApplication()
-        let tablesQuery = app.tables
-        tablesQuery.textViews.containingType(.StaticText, identifier:"Enter your intranet URL").element.tap()
-        tablesQuery.textViews.staticTexts["Enter your intranet URL"].tap()
+        
+        //--- Add new server button
+        let newServerButton = app.buttons["button.new.server"]
+        XCTAssert(newServerButton.exists)
+        newServerButton.tap()
+        XCTAssertEqual(app.tables.count,1)
+        //--- Server URL area
+        let serverNameText = app.textViews.staticTexts["Enter your intranet URL"]
+        XCTAssert(serverNameText.exists)
+        
+        //--- Add server URL
+        serverNameText.tap()
         app.typeText("https://community.exoplatform.com\n")
-        _ = self.expectationForPredicate(
-            NSPredicate(format: "count == 1"),
-            evaluatedWithObject: app.webViews,
-            handler: nil)
+        
+        
+        self.expectationForPredicate(NSPredicate(format: "count == 0"), evaluatedWithObject: app.webViews, handler: nil)
+        
+        
         self.waitForExpectationsWithTimeout(100.0) { (error) -> Void in
             if error != nil {
-                XCTFail("Expect webview to be shown")
+                 XCTFail("Expect webview to be shown")
             }
         }
+        
     }
- */
- /**
+    
+    func testAddServerNotSupport() {
+        
+        let app = XCUIApplication()
+        app.buttons["button.new.server"].tap()
+        
+        //--- Server URL area
+        let serverNameText = app.textViews.staticTexts["Enter your intranet URL"]
+        XCTAssert(serverNameText.exists)
+        
+        // TODO: add a request interceptor to avoid hitting the real server, and return a predefined response
+        //--- Add server URL
+        serverNameText.tap()
+        app.typeText("https://exoplatform.exoplatform.net\n")
+        let alert = app.alerts.elementBoundByIndex(0)
+        self.expectationForPredicate(NSPredicate (format: "exists == 1", argumentArray: nil), evaluatedWithObject: alert, handler: nil)
+        self.waitForExpectationsWithTimeout(100.0) { (error) -> Void in
+            if error == nil {
+                XCTAssertEqual(alert.label, "Intranet URL error")
+                let okButton = alert.buttons["OK"]
+                okButton.tap()
+            } else {
+                XCTFail("Expect alertview to be shown")
+            }
+        }
+        /**
+        
+        app.alerts["Platform version not supported"].collectionViews.buttons["OK"].tap()
+        
+        app.navigationBars["Sign in to eXo"].buttons["Back"].tap()
+        */
+        
+        /* */
+    
+    }
+    
+    /**
     func testAddServerNotSupport() {
         
         self.gotoAddServerScreen()
