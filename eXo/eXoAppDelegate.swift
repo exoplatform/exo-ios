4fd7ece4f75a35622732142920394e4e35399da2
Wajih Benabdessalem <wbenabdessalem@exoplatform.com>
2021-06-14 14:17:45 +0100

    45213 : SignIn_To_eXo_US

--- eXo/eXoAppDelegate.swift
+++ eXo/eXoAppDelegate.swift
@@ -85,6 +85,12 @@ class eXoAppDelegate: UIResponder, UIApplicationDelegate, MessagingDelegate, UNU
         navigationVC?.pushViewController(homepage! as UIViewController, animated: false)
     }
 
+    func setRootToConnect(){
+        let signInToeXo = ConnectToExoViewController(nibName: "ConnectToExoViewController", bundle: nil)
+        navigationVC?.navigationBar.isHidden = false
+        navigationVC?.pushViewController(signInToeXo, animated: false)
+    }
+    
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
