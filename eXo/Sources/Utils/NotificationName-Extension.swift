a8be3ef3ec68fd80d5c64f266f7e6aeb8e798c3f
wbenabdessalem <82816251+wbenabdessalem@users.noreply.github.com>
2021-06-22 15:46:33 +0100

    45913 : Feature/us remove shadow card (#58)
    * Initialize CI/CD for acceptance environment
    
    * Feature/add delete on card  (#55)
    
    * Initialize CI/CD for acceptance environment
    
    * 45212 : SignIn_To_eXo_US_06 : Add delete icon on card (to Acceptance branch)
    
    Co-authored-by: Houssem Ben Ali <hbenali@exoplatform.com>
    Co-authored-by: Ali HAMDI <ahamdi@exoplatform.com>
    
    * 45197 : AddingURL_US_05
    
    * 45904 : Delete header of instance page
    
    * 45913 : Delete shadow in card from instance page
    
    * test author change
    
    * update author test
    
    * Resolve conflict
    
    * group update to <string>group.$(APP_BUNDLE_ID)</string>
    
    * update group share ext to <string>group.$(APP_BUNDLE_ID)</string>
    
    Co-authored-by: Houssem Ben Ali <hbenali@exoplatform.com>
    Co-authored-by: Ali HAMDI <ahamdi@exoplatform.com>
    Co-authored-by: Wajih Benabdessalem <wajihbenabdessalem@Wajihs-MacBook-Pro.local>
    (cherry picked from commit b678a6b4a5141e03b26eb2d568ee5bfdec966b92)

---
+++ eXo/Sources/Utils/NotificationName-Extension.swift
@@ -0,0 +1,13 @@
+//
+//  NotificationName-Extension.swift
+//  eXo
+//
+//  Created by Wajih Benabdessalem on 20/6/2021.
+//  Copyright © 2021 eXo. All rights reserved.
+//
+
+import Foundation
+
+extension Notification.Name {
+    public static let addDomainKey = Notification.Name(rawValue: "AddDomainKey")
+}
