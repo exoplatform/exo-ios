f59865cb178c1a7bbaf25395731c90dc059f0807
Wajih Benabdessalem <wbenabdessalem@exoplatform.com>
2022-01-17 15:37:36 +0100

    53241 : Launch in web problem

--- eXo/Sources/Controllers/HomePage/HomePageViewController.swift
+++ eXo/Sources/Controllers/HomePage/HomePageViewController.swift
@@ -317,6 +317,7 @@ class HomePageViewController: eXoWebBaseController, WKNavigationDelegate, WKUIDe
         configuration.allowsAirPlayForMediaPlayback = true
         configuration.allowsPictureInPictureMediaPlayback = true
         configuration.mediaTypesRequiringUserActionForPlayback = []
+        configuration.applicationNameForUserAgent = "Version/15.0 Mobile/15E148 Safari/604.1"
         popupWebView = WKWebView(frame: .zero, configuration: configuration)
         popupWebView?.navigationDelegate = self
         popupWebView?.uiDelegate = self
