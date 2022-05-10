47c788ad82d6f31667a3f858ec453c1dfe742332
wbenabdessalem <82816251+wbenabdessalem@users.noreply.github.com>
2022-05-10 15:57:07 +0100

    Task-57048: Remove All unused Code and Files from the iOS app (#201)

--- eXo/Sources/Controllers/HomePage/HomePageViewController.swift
+++ eXo/Sources/Controllers/HomePage/HomePageViewController.swift
@@ -268,7 +268,7 @@ final class HomePageViewController: eXoWebBaseController, WKNavigationDelegate, 
         
         if response.url?.absoluteString.range(of: serverDomain!+"/portal/rest/state/status") != nil  {
             if (response.statusCode >= 200  && response.statusCode < 300) {
-                self.showOnBoardingIfNeed()
+                print("In Session")
             }
             decisionHandler(.cancel)
             return
