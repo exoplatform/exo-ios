033bf803e26fda512e5958ed4c94dad2a5944cb8
wbenabdessalem <82816251+wbenabdessalem@users.noreply.github.com>
2022-05-23 16:38:35 +0100

    Task-57149: when I click on Add a drive icon; error page is displayed (#202)
    * Task-57149: when I click on Add a drive icon; error page is displayed
    
    * Task-57149: Update suggestion changes

--- eXo/Sources/Controllers/HomePage/HomePageViewController.swift
+++ eXo/Sources/Controllers/HomePage/HomePageViewController.swift
@@ -247,9 +247,9 @@ final class HomePageViewController: eXoWebBaseController, WKNavigationDelegate, 
          Request to /rest/state/status to check if user has connected?: 300> status code >=200 --> Connected
          */
         
-        if response.url?.absoluteString.range(of: serverDomain!+"/rest/state/status") != nil  {
+        if response.url?.absoluteString.range(of: serverDomain!+"/portal/rest/state/status") != nil  {
             if (response.statusCode >= 200  && response.statusCode < 300) {
-                print("In Session")
+                self.showOnBoardingIfNeed()
             }
             decisionHandler(.cancel)
             return
@@ -367,6 +367,7 @@ final class HomePageViewController: eXoWebBaseController, WKNavigationDelegate, 
                 newWebview.topAnchor.constraint(equalTo: self.webViewContainer.topAnchor),
                 newWebview.bottomAnchor.constraint(equalTo: self.webViewContainer.bottomAnchor)
             ])
+            newWebview.load(navigationAction.request)
         }
         return popupWebView ?? nil
     }
@@ -400,23 +401,33 @@ final class HomePageViewController: eXoWebBaseController, WKNavigationDelegate, 
     }
     
     /*
-     Ask to load the page <serverURL>/rest/state/status
-     - If the user has connected the response status code of this request = 200
-     */
-    func loadStateStatusPage () {
-        guard let serverUrl = self.serverURL, let serverDomain = URL(string: serverUrl)?.host else { return }
-        if self.webView?.url!.absoluteString.range(of: serverDomain + "/portal/intranet") != nil  {
-            let statusURL = serverUrl.serverDomainWithProtocolAndPort! + "/rest/state/status"
-            let url = URL(string: statusURL)
-            let request = URLRequest(url: url!, cachePolicy: .useProtocolCachePolicy, timeoutInterval: Config.timeout)
-            self.webView?.load(request)
-        }
-    }
-    
+       Display the Connect page View Controller if:
+       - The view has never been shown
+       - After user has logged in
+       */
+       func showOnBoardingIfNeed () {
+           if (UserDefaults.standard.object(forKey: Config.onboardingDidShow) == nil){
+               UserDefaults.standard.set(NSNumber(value: true as Bool), forKey: Config.onboardingDidShow)
+               let appDelegate = UIApplication.shared.delegate as! eXoAppDelegate
+               appDelegate.handleRootConnect()
+           }
+       }
+       /*
+        Ask to load the page <serverURL>/rest/state/status
+        - If the user has connected the response status code of this request = 200
+        */
+       func loadStateStatusPage() {
+           guard let serverUrl = self.serverURL, let serverDomain = URL(string: serverUrl)?.host else { return }
+           if self.webView?.url!.absoluteString.range(of: serverDomain + "/portal/dw") != nil  {
+               let statusURL = serverUrl.serverDomainWithProtocolAndPort! + "/portal/rest/state/status"
+               let url = URL(string: statusURL)
+               let request = URLRequest(url: url!, cachePolicy: .useProtocolCachePolicy, timeoutInterval: Config.timeout)
+               self.webView?.load(request)
+           }
+       }
     /*
      Get the remote avatar image, this need credentials in call .
      */
-    
     func saveLogoDomain(url:URL,cookies:[HTTPCookie]){
         if url.absoluteString.contains("/portal/dw") {
             let logoEndPoint = "/portal/rest/v1/platform/branding/logo"
